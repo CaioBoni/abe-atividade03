e2fc975bb641ce05f51adf9144eb16ea9195b99b
Caio <you@example.com>
2019-09-24 22:07:44 -0300

    inclusao validador

--- aula03-rest-service/src/main/java/aula03/controller/LivroController.java
+++ aula03-rest-service/src/main/java/aula03/controller/LivroController.java
@@ -3,27 +3,32 @@ package aula03.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import aula03.entity.Livro;
 import aula03.service.LivroService;
+import aula03.service.ValidadorTokenService;
 
 @RestController
 public class LivroController {
 	
 	@Autowired LivroService livroService;
+	@Autowired ValidadorTokenService validadorTokenService;
 	
 	@GetMapping(value = "/livros", produces = "application/json")
-	public List<Livro> buscarLivros(@RequestBody Livro livro) {
-		return livroService.buscarLivros(livro);
+	public ResponseEntity<List<Livro>> buscarLivros(@RequestBody Livro livro) {
+		if (validadorTokenService.validar()) {
+			return new ResponseEntity<>(livroService.buscarLivros(livro), HttpStatus.OK);
+		}
+		return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
 	}
 
 	@PostMapping(value = "/livros", produces = "application/json")
@@ -40,6 +45,12 @@ public class LivroController {
 	public Livro deletarLivro(@RequestParam Long id) {
 		return livroService.deletarLivro(id);
 	}
-
-//	Livro.comComentario(livro.getComentarios()
+	
+	@PostMapping(value = "/livros/audit", produces = "application/json")
+	public ResponseEntity<?> cadastrarLivros(@RequestBody Livro livro) {
+		if (validadorTokenService.validar()) {
+			return new ResponseEntity<>(livroService.registrarAuditoria(livro), HttpStatus.OK);
+		}
+		return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+	}
 }
--- aula03-rest-service/src/main/java/aula03/service/LivroService.java
+++ aula03-rest-service/src/main/java/aula03/service/LivroService.java
@@ -3,14 +3,16 @@ package aula03.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.client.RestTemplate;
 
 import aula03.entity.Livro;
 
 @Service
 public class LivroService {
-
+	
 	public List<Livro> buscarLivros(@RequestBody Livro livro) {
 		return null;
 	}
@@ -27,6 +29,12 @@ public class LivroService {
 	
 	public Livro deletarLivro(Long id) {
 		return new Livro();
+	}
+	
+	public ResponseEntity<?> registrarAuditoria(Livro livro) {
+		RestTemplate template = new RestTemplate();
+		ResponseEntity<?> response = template.postForEntity("http://localhost:8080/api/shopping", livro, Object.class);
+		return response;
 	}
 
 }
